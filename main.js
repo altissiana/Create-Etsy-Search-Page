50a4c7b95364e9c37852f5933df9093d658f227e
Tissiana <laratissiana@gmail.com>
2019-03-25 08:56:12 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,222 @@
+
+var data = [
+    {
+        name: "Fully Handmade Gold Plated Real Tachi Sword Japanese Samurai Sword",
+        desc: "TrueKatana",
+        img: "https://i.etsystatic.com/19199040/r/il/399f8b/1767533572/il_570xN.1767533572_cw9o.jpg",
+        price: "$912.00"
+    },
+    {
+        name: "WW2 Type 98 Shin Gunto Japanese Officer Samurai Sword Clay Tempered 1095 Steel Katana",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/493e37/1787739676/il_570xN.1787739676_dwkx.jpg",
+        price: "$839.99"
+    },
+    {
+        name: "Skyjiro Hota Tegai Japnese samurai sword katana",
+        desc: "lotussamurai",
+        img: "https://i.etsystatic.com/15382851/r/il/ac339a/1561873667/il_570xN.1561873667_5ke8.jpg",
+        price: "$880.26"
+    },
+    {
+        name: "Skyjiro hota tegai japanese samurai sword katana",
+        desc: "lotussamurai",
+        img: "https://i.etsystatic.com/15382851/r/il/59db52/1597753036/il_570xN.1597753036_8z4z.jpg",
+        price: "$828.87"
+    },{
+        name: "Skyjiro Hanabira Japanese Samurai Sword Katana",
+        desc: "lotussamurai",
+        img: "https://i.etsystatic.com/15382851/r/il/bfe6c9/1590992230/il_570xN.1590992230_ndeh.jpg",
+        price: "$608.62"
+    },{
+        name: "Samurai Sword Guard - FREE SHIPPING",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/40791d/1427061817/il_570xN.1427061817_4ocx.jpg",
+        price: "$1,225.00"
+    },{
+        name: "Samurai Sword Guard - FREE SHIPPING",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/dec4bd/1379788480/il_570xN.1379788480_dceo.jpg",
+        price: "$1,275.00"
+    },{
+        name: "Samurai Sword Guard - FREE SHIPPING",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/29470a/1427062751/il_570xN.1427062751_g2wa.jpg",
+        price: "$550.00"
+    },{
+        name: "Samurai Sword Guard - FREE SHIPPING",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/80beb5/1379790260/il_570xN.1379790260_cskl.jpg",
+        price: "$875.00"
+    },{
+        name: "Handmade Gold Sun Tsuba Fish Skin Real Tachi Sword Japanese Samurai Swords",
+        desc: "TrueKatana",
+        img: "https://i.etsystatic.com/19199040/r/il/2428aa/1767532160/il_570xN.1767532160_g749.jpg",
+        price: "$780.00"
+    },{
+        name: "TSUBA Antique Japanese sword guard 'Elderly couple' Edo era samurai Tsuba",
+        desc: "ARTradeJAPAN",
+        img: "https://i.etsystatic.com/9507245/r/il/17f88c/1258220843/il_570xN.1258220843_qlcv.jpg",
+        price: "$2,254.62"
+    },{
+        name: "Samurai Sword Guard - FREE SHIPPING",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/47d7f6/1427062085/il_570xN.1427062085_f6n1.jpg",
+        price: "$775.00"
+    },{
+        name: "Skyjiro hanabira japanese samurai sword katana iaito",
+        desc: "lotussamurai",
+        img: "https://i.etsystatic.com/15382851/r/il/e0d31d/1638430421/il_570xN.1638430421_tgze.jpg",
+        price: "$557.23"
+    },{
+        name: "Hand Forged High Quality Chinese Tamahagane Clay Tempered Samurai Katana Sword",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/151024/1777954105/il_570xN.1777954105_qc2i.jpg",
+        price: "$3,689.99"
+    },{
+        name: "Japanese Samurai sword Kozuka Kogatana Tsuba shibuichi silver Kashi Mon Edo period 19th century",
+        desc: "ANTIQUEFORCE",
+        img: "https://i.etsystatic.com/15612601/r/il/57a8d9/1695215082/il_570xN.1695215082_fck5.jpg",
+        price: "$799.00"
+    },{
+        name: "Military Samurai Estate Lot 4 Sliver Knights Decorated Masonic Swords by MC Lilley Columbus Ohio with Scabbard Covers FREE SHIPPING",
+        desc: "fifiAttic",
+        img: "https://i.etsystatic.com/11456693/r/il/f3dc63/1815846720/il_570xN.1815846720_bzp3.jpg",
+        price: "$936.25"
+    },{
+        name: "TSUBA Samurai Shogun Sword Katana Nihontou Mounting Art Sengoku Japanese Antique",
+        desc: "MoreJapan",
+        img: "https://i.etsystatic.com/9297956/r/il/e4dd9d/645977140/il_570xN.645977140_7jwa.jpg",
+        price: "$599.00"
+    },{
+        name: "WW2 Shin Gunto Type 98 Japanese Officer Samurai Sword Clay Tempered 1095 Steel Katana",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/60575b/1787735840/il_570xN.1787735840_c4e0.jpg",
+        price: "$839.99"
+    },{
+        name: "Antique Hand Forged Iron Japanese Samurai Sword Guard from Edo Period (1603-1868)",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/943912/1379802198/il_570xN.1379802198_7q2p.jpg",
+        price: "$1,000.00"
+    },{
+        name: "Antique Hand Forged Iron Japanese Samurai Sword Guard from Edo Period (1603-1868)",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/746d84/1427074985/il_570xN.1427074985_lcmm.jpg",
+        price: "$1,175.00"
+    },{
+        name: "Handmade Gold Flower Tsuba Real Katana Japanese Samurai Swords",
+        desc: "TrueKatana",
+        img: "https://i.etsystatic.com/19199040/r/il/c7ee5b/1815152745/il_570xN.1815152745_39pa.jpg",
+        price: "$743.00"
+    },{
+        name: "Handmade Flower Blade Katana Real Katana Japanese Samurai Swords With T10 Clip Steel",
+        desc: "TrueKatana",
+        img: "https://i.etsystatic.com/19199040/r/il/213212/1815157023/il_570xN.1815157023_rgaq.jpg",
+        price: "$728.00"
+    },{
+        name: "Twinblade 1095 High Carbon Steel Clay Tempered Double Blade Crescent Glaive Wakizashi Samurai Sword",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/069e09/1759081276/il_570xN.1759081276_ls9p.jpg",
+        price: "$639.99"
+    },{
+        name: "1095 High Carbon Steel Blade Clay Tempered Samurai Hand Forged Battle Ready Japanese Sword Set Daisho Katana, Wakizashi & Tanto",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/353b87/1708669499/il_570xN.1708669499_6b0f.jpg",
+        price: "$1,129.99"
+    },{
+        name: "Black Folded Damascus Steel Hand Forged Japanese Samurai Ninjato Sword Gemini Twin Straight-Edge Blade Katana Ninja Set",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/c839ef/1661198810/il_570xN.1661198810_qtdx.jpg",
+        price: "$539.99"
+    },{
+        name: "Handmade Gold Lion Plated Fish Skin Real Tachi Sword Japanese Samurai Swords",
+        desc: "TrueKatana",
+        img: "https://i.etsystatic.com/19199040/r/il/bc63aa/1767531666/il_570xN.1767531666_tm8u.jpg",
+        price: "$624.99"
+    },{
+        name: "Westworld Miyamoto Musashi Shogun Hand Forged 1095 High Carbon Steel Blade Battle Ready Sword Clay Tempered Japanese Samurai Katana",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/872f96/1661191774/il_570xN.1661191774_mqt0.jpg",
+        price: "$939.99"
+    },{
+        name: "Antique Hand Forged Iron Japanese Samurai Sword Guard from Edo Period (1603-1868)",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/94c3c4/1379797368/il_570xN.1379797368_b01f.jpg",
+        price: "$1,550.00"
+    },{
+        name: "Samurai Ring",
+        desc: "TheAntiqueJeweler",
+        img: "https://i.etsystatic.com/7098573/r/il/e50de4/1621593929/il_570xN.1621593929_45wb.jpg",
+        price: "$980.00"
+    },{
+        name: "Sasaki Kojiro Odachi Clay Tempered 1095 Steel Nodachi Dark Souls 2 Washing Pole Monohoshizao Sword",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/13a782/1790660435/il_570xN.1790660435_h2sx.jpg",
+        price: "$739.99"
+    },{
+        name: "Antique Hand Forged Iron Japanese Samurai Sword Guard from Edo Period (1603-1868)",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/5b80cb/1379800746/il_570xN.1379800746_73vw.jpg",
+        price: "$1,275.00"
+    },{
+        name: "TSUBA Antique Japanese sword guard 'Ichime gasa Noble lady's hat' with an expert written statement Edo era samurai Tsuba",
+        desc: "ARTradeJAPAN",
+        img: "https://i.etsystatic.com/9507245/r/il/d3a0ef/1217920936/il_570xN.1217920936_3yhk.jpg",
+        price: "$939.43"
+    },{
+        name: "Antique Hand Forged Iron Japanese Samurai Sword Guard from Edo Period (1603-1868)",
+        desc: "Shibuikotto",
+        img: "https://i.etsystatic.com/8448968/r/il/445476/1427071015/il_570xN.1427071015_h9cv.jpg",
+        price: "$600.00"
+    },{
+        name: "Samurai Sword Katana Handmade Damascus steel blade Brass fittings Skin scabbard",
+        desc: "HandmadeSwordArt",
+        img: "https://i.etsystatic.com/19530950/r/il/da22eb/1833101145/il_570xN.1833101145_2upg.jpg",
+        price: "$609.00"
+    },{
+        name: "Hand Forged Clay Tempered 1095 High Carbon Steel Japanese Samurai Tachi Sword",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/a9f84b/1730493414/il_570xN.1730493414_mslw.jpg",
+        price: "$819.99"
+    },{
+        name: "Hand Forged 1095 High Carbon Steel Black Blade and Red Folded Steel Battle Ready Japanese Samurai Katana Sword Set",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/9b77d0/1661182860/il_570xN.1661182860_cfdo.jpg",
+        price: "$684.99"
+    },{
+        name: "O-Kissaki Deep Sori Shinogi-Zukuri Handmade Clay Tempered 1095 Steel Samurai Katana Sword",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/9c1e80/1730491080/il_570xN.1730491080_ppg7.jpg",
+        price: "$869.99"
+    },{
+        name: "1095 High Carbon Steel Clay Tempered Katana with Hand Carved Saya & Hidden Kozuka Knife",
+        desc: "SwordofNorthshire",
+        img: "https://i.etsystatic.com/18769670/r/il/f94716/1781115569/il_570xN.1781115569_25tq.jpg",
+        price: "$619.99"
+    },
+]
+
+var itemcontainer = document.querySelector('#itemcontainer')
+
+
+var template = `
+<div class="item">
+<img src="" style="width:100%" />
+<p class="name"></p>
+<p class="desc"></p>
+<p class="price"></p>
+</div>
+`
+
+var htmlstr = data.map(item =>{
+    return `
+    <div class="item">
+    <img src="${item.img}" style="width:100%"  />
+    <p class="name">${item.name}</p>
+    <p class="desc">${item.desc}</p>
+    <p class="price">${item.price}</p>
+    </div>
+    `
+}).join("")
+
+itemcontainer.innerHTML = htmlstr
